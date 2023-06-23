a8d7d48b1c3903d2aa9b7719da8173d86ffc0454
laigasus <okjaeook98@gmail.com>
2023-06-23 22:03:48 +0900

    update Singleton

--- DesignPattern/Singleton/Main.java
+++ DesignPattern/Singleton/Main.java
@@ -1,6 +1,14 @@
 package Singleton;
 
 public class Main {
+    enum EnumSingleton {
+        INSTANCE;
+
+        public void hello() {
+            System.out.println("enum을 이용한 Singleton");
+        }
+    }
+
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
         Singleton s1 = Singleton.getInstance();
@@ -10,5 +18,7 @@ public class Main {
         sb.append(s1 == s2 ? "같은" : "다른");
         sb.append(" 인스턴스입니다");
         System.out.println(sb);
+
+        EnumSingleton.INSTANCE.hello();
     }
 }
--- DesignPattern/Singleton/Singleton.java
+++ DesignPattern/Singleton/Singleton.java
@@ -3,6 +3,7 @@ package Singleton;
 public class Singleton {
     private static Singleton singleton = new Singleton();
 
+    // 클래스 외부에서 new 되지 않도록 private 생성자 선언
     private Singleton() {
         System.out.println("Singeton 인스턴스 생성");
     }
