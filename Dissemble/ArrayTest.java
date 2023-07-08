41f3bcca365ff1a8ff7293b729e2f779ceb0e99d
laigasus <okjaeook98@gmail.com>
2023-07-08 11:47:20 +0900

    Dissemble

---
+++ Dissemble/ArrayTest.java
@@ -0,0 +1,14 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+        test1();
+        test2();
+    }
+
+    static void test1() {
+        int[] array = new int[] { 1, 2, 3 };
+    }
+
+    static void test2() {
+        int[] array = { 1, 2, 3 };
+    }
+}
